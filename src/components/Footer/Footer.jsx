61aff7caaf377d829ccacf0b91fc3b9788173296
Cez Nicole Comia <cezcomia@gmail.com>
2024-05-30 18:41:06 -0400

    create footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer(){
+    return(
+        <>
+            <section className="footer__section">
+                <li className="footer__list-item">
+                    <ul className="footer__link-group">
+                        <img src="" alt="home icon" className="footer__icon" /><a href="/"></a>
+                        <img src="" alt="search icon" className="footer__icon" /><a href="/"></a>
+                        <img src="" alt="adjacent left and right arrow icon" className="footer__icon" /><a href="/"></a>
+                        <img src="" alt="clock icon" className="footer__icon" /><a href="/"></a>
+                    </ul>
+                </li>
+            </section>
+       </>
+    )
+}
