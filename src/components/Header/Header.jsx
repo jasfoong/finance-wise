6f9b3c147635e9898c16dfca6b450442b47913cc
Sufia Ali <sufiaali@Sufias-MacBook-Air-2.local>
2024-05-30 22:23:07 -0400

    changed the Header function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "../../assets/icons/Wealthsimple.svg";
+import user from "../../assets/icons/user.svg";
+import gift from "../../assets/icons/gifts.svg";
 import "./Header.scss"
 
-const InteractionPage = () => {
+const Header = () => {
     return (
+        <header>
+            <div className="header__logo">
+                <Link to="/">
+                    <img src={logo} alt="Logo" className="header__logo-image" />
+                </Link>
+            </div>
+            <div className="header__icon">
+                <Link to="/">
+                    <img src={gift} alt="Gift" className="header__icon-gift" />
+                </Link>
+            </div>
+            <div className="header__icon">
+                <Link to="/">
+                    <img src={user} alt="User" className="header__icon-user" />
+                </Link>
+            </div>
+        </header>
     )
 };
 
-export default InteractionPage;
+export default Header;
