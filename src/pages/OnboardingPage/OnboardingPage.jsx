2fb65deba93f43a2b8f7447137344722236e155c
Jasmine Foong <jasfoong19@gmail.com>
2024-05-30 21:22:03 -0700

    RandomizedTips component that displays random item from an array of hardcoded tips; plus basic styling

--- src/pages/OnboardingPage/OnboardingPage.jsx
+++ src/pages/OnboardingPage/OnboardingPage.jsx
@@ -1,10 +1,12 @@
 import "./OnboardingPage.scss"
 import Header from "../../components/Header/Header"
+import RandomizedTips from "../../components/RandomizedTips/RandomizedTips";
 
 const OnboardingPage = () => {
     return (
         <>
         <Header />
+        <RandomizedTips />
         <h1>onboarding page goes here</h1>
         </>
     )
