d97da5e1e7fe28de72919b41fcb7319a5fac2f28
Cez Nicole Comia <cezcomia@gmail.com>
2024-05-30 22:45:25 -0400

    modify footer to change active link image

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,37 @@
 import homeIcon from "../../assets/icons/home-icon.png";
+import activeHomeIcon from "../../assets/icons/active-home-icon.png";
 import searchIcon from "../../assets/icons/search-icon.png";
+import activeSearchIcon from "../../assets/icons/active-search-icon.png";
 import arrowsIcon from "../../assets/icons/adjacent-arrows-icon.png";
+import activeArrowsIcon from "../../assets/icons/active-adjacent-arrows-icon.png";
 import clockIcon from "../../assets/icons/clock-icon.png";
 import "./Footer.scss";
+import { useState } from "react";
 
 
 export default function Footer(){
-    return(
+    const [activeLink, setActiveLink] = useState(window.location.pathname);
+
+    const handleLinkClick = (link) => {
+        setActiveLink(link);
+    };
+
+    return (
         <section className="footer__section">
             <li className="footer__list-item">
                 <ul className="footer__link-group">
-                    <img src={homeIcon} alt="home icon" className="footer__icon" /><a href="/"></a>
-                    <img src={searchIcon} alt="search icon" className="footer__icon" /><a href="/"></a>
-                    <img src={arrowsIcon} alt="adjacent left and right arrow icon" className="footer__icon" /><a href="/"></a>
-                    <img src={clockIcon} alt="clock icon" className="footer__icon" /><a href="/"></a>
+                    <a href="/" className="footer__link" onClick={() => handleLinkClick('/')}>
+                        <img src={activeLink === '/' ? activeHomeIcon : homeIcon} alt="home icon" className="footer__icon" />
+                    </a>
+                    <a href="/account" className="footer__link" onClick={() => handleLinkClick('/account')}>
+                        <img src={activeLink === '/account' ? activeSearchIcon : searchIcon} alt="search icon" className="footer__icon" />
+                    </a>
+                    <a href="/account" className="footer__link" onClick={() => handleLinkClick('/account')}>
+                        <img src={activeLink === '/account' ? activeArrowsIcon : arrowsIcon} alt="adjacent left and right arrow icon" className="footer__icon" />
+                    </a>
+                    <a href="/account" className="footer__link">
+                        <img src={clockIcon} alt="clock icon" className="footer__icon" />
+                    </a>
                 </ul>
             </li>
         </section>
