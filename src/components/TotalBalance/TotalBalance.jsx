3e5466f89097ab1648eb864963cd260c7d4b7542
Cez Nicole Comia <cezcomia@gmail.com>
2024-05-31 12:06:29 -0400

    install mui and create TotalBalance component

--- src/components/TotalBalance/TotalBalance.jsx
+++ src/components/TotalBalance/TotalBalance.jsx
@@ -1,8 +1,44 @@
 import "./TotalBalance.scss"
 
-const TotalBalance = () => {
+const TotalBalance = ({categoryBalance}) => {
+    const moneyCategories = [
+        "rent/mortgage", 
+        "bills",
+        "transportation",
+        "food",
+        "health",
+        "subscriptions",
+        "fun",
+        "savings",
+        "investments"
+    ];
+
+    const totalBalance = Object.values(categoryBalance).reduce((total, value) => {
+        const amount = parseFloat(value) || 0;
+        return total + amount;
+    })
+
+    const categoryBalances = moneyCategories.map(category => {
+        return(
+            <li key={category}>
+                {category}: {categoryBalance[category]}
+            </li>
+        )
+    })
+
     return (
-        <h1>total balance shows here</h1>
+        <>
+            <section className="total-balance__overall">
+                <h1 className="total-balance__title">Total Balance: ${totalBalance}</h1>
+            </section>
+
+            <section className="total-balance__categories">
+                <h2 className="total-balance__title">Total Category Balances:</h2>
+                <ul>
+                    {categoryBalances}
+                </ul>
+            </section>
+        </>
     )
 };
 
--- src/pages/InteractionPage/InteractionPage.jsx
+++ src/pages/InteractionPage/InteractionPage.jsx
@@ -1,11 +1,13 @@
 import "./InteractionPage.scss";
 import Header from "../../components/Header/Header";
 import Chart from "../../components/Chart/Chart";
+import TotalBalance from "../../components/TotalBalance/TotalBalance";
 
-const InteractionPage = () => {
+const InteractionPage = ({categoryBalance}) => {
   return (
     <>
       <Header />
+      <TotalBalance categoryBalance={categoryBalance} />
       <h1>interaction page goes here</h1>
       <Chart />
     </>
