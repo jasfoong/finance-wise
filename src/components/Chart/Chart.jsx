24ae8284ed50df3d2bdf0f28c3b8e996799ad6f0
tstidwill <tara.stidwill@gmail.com>
2024-05-31 10:27:28 -0400

    "Add more simulation events"

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,28 +4,37 @@ import { useState } from "react";
 
 export default function Chart() {
   const [accountBalanceData, setAccountBalanceData] = useState([
-    { x: 0, y: 5000 },
-    { x: 1, y: -5000 },
+    { x: 0, y: -5000 },
+    { x: 1, y: 3000 },
   ]);
 
-  const changeEachDay = 100; //formula here
-  const surpriseCost = -500; // formula here
-  const surpriseGain = +500; //formula here
+  const changeEachDay = -200; //formula here like expenses /30?
+  const surpriseCost = -3000; //  fixed amount?
+  const surpriseGain = +2000; //formula here like (investements+savings)* 1.25
+  const income = +3000; //formula/ input?
 
   function clickHandler() {
     const newData = [...accountBalanceData];
     const lastDataPoint = newData[newData.length - 1];
     const newDay = lastDataPoint.x + 1;
 
+    console.log(`everyday expenses`, changeEachDay);
     let newBalance = lastDataPoint.y + changeEachDay;
 
-    if (newDay % 5 === 0) {
-      newBalance += surpriseGain;
-    }
     if (newDay % 3 === 0) {
+      newBalance += surpriseGain;
+      console.log(`surprise gain: `, surpriseGain);
+    }
+    if (newDay % 7 === 0) {
       newBalance += surpriseCost;
+      console.log(`surprise expense: `, surpriseCost);
+    }
+    if (newDay % 5 === 0) {
+      newBalance += income;
+      console.log(`pay day: `, income);
     }
 
+    console.log({ x: newDay, y: newBalance });
     newData.push({ x: newDay, y: newBalance });
 
     setAccountBalanceData(newData);
