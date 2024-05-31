a04f872bc14027d4caf510bb395445a67082ec22
tstidwill <tara.stidwill@gmail.com>
2024-05-31 15:33:48 -0400

    remove placeholder txt and console log

--- src/pages/InteractionPage/InteractionPage.jsx
+++ src/pages/InteractionPage/InteractionPage.jsx
@@ -1,14 +1,10 @@
 import "./InteractionPage.scss";
 import Header from "../../components/Header/Header";
 import Chart from "../../components/Chart/Chart";
-import TotalBalance from "../../components/TotalBalance/TotalBalance";
-import { useEffect } from "react";
 
 import Pie from "../../components/Pie/Pie";
 
 const InteractionPage = ({ totalBalance, categoryBalance }) => {
-  console.log(categoryBalance); //access balance as needed
-
   return (
     <>
       <Header />
