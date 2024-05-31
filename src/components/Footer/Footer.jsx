485e6730f39ffc5f62ee76e715e2022d913ba279
Cez Nicole Comia <cezcomia@gmail.com>
2024-05-30 23:06:26 -0400

    update active icon assets and display active icons on active routes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import activeSearchIcon from "../../assets/icons/active-search-icon.png";
 import arrowsIcon from "../../assets/icons/adjacent-arrows-icon.png";
 import activeArrowsIcon from "../../assets/icons/active-adjacent-arrows-icon.png";
 import clockIcon from "../../assets/icons/clock-icon.png";
+import activeClockIcon from "../../assets/icons/active-clock-icon.png";
 import "./Footer.scss";
 import { useState } from "react";
 
@@ -30,7 +31,7 @@ export default function Footer(){
                         <img src={activeLink === '/account' ? activeArrowsIcon : arrowsIcon} alt="adjacent left and right arrow icon" className="footer__icon" />
                     </a>
                     <a href="/account" className="footer__link">
-                        <img src={clockIcon} alt="clock icon" className="footer__icon" />
+                        <img src={activeLink === '/account' ? activeClockIcon : clockIcon} alt="clock icon" className="footer__icon" />
                     </a>
                 </ul>
             </li>
